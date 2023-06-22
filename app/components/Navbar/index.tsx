212940c9386e2f55c15fe635f936640344ffa4a9
Thiago <thiago@pop-os.localdomain>
2023-06-22 08:30:50 -0300

    fist commit

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { Code } from '@phosphor-icons/react';
+import React from 'react';
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div className='w-full flex justify-between items-center'>
+            <div className='flex justify-center items-center gap-2 py-4'>
+                <div className='bg-white rounded-md p-2'>
+                    <Code className=' text-[#383838]' weight='bold' size={18} />
+                </div>
+                <span className='font-semibold text-lg'>Thiago Boize</span>
+            </div>
+            <div className='flex justify-center items-center gap-x-4'>
+                <Link href={'#anchor'}>Home</Link>
+                <Link href={'#anchor'}>Sobre</Link>
+                <Link href={'#anchor'}>Projetos</Link>
+                <Link href={'#anchor'}>Blog</Link>
+            </div>
+            <button className=' transition-all font-semibold text-transparent  bg-clip-text bg-gradient-to-br from-[#00FEFC] to-[#13BB15] p-1 hover:scale-105' >
+              Contate-me
+            </button>
+        </div>
+    );
+}
+
+export default Navbar;
